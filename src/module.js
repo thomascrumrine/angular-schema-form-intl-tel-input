2ab3259345f1710f81ad4d612f56b050af5d2467
thomascrumrine <tommy@apruve.com>
2017-11-17 15:27:46 -0600

    minify errors

--- src/module.js
+++ src/module.js
@@ -1,15 +1,14 @@
-angular.module('intlTelInput', [
-  'schemaForm',
-  'templates',
-  'ngIntlTelInput'
-]).config(function(schemaFormDecoratorsProvider, sfBuilderProvider, ngIntlTelInputProvider) {
+angular.module('intlTelInput', ['schemaForm', 'templates', 'ngIntlTelInput']).config([
+  'schemaFormDecoratorsProvider', 'sfBuilderProvider', 'ngIntlTelInputProvider',
+  function(schemaFormDecoratorsProvider, sfBuilderProvider, ngIntlTelInputProvider) {
 
-  schemaFormDecoratorsProvider.defineAddOn(
-    'bootstrapDecorator',           // Name of the decorator you want to add to.
-    'intl_tel_input',                      // Form type that should render this add-on
-    'src/templates/intl_tel_input.html',  // Template name in $templateCache
-    sfBuilderProvider.stdBuilders   // List of builder functions to apply.
-  )
+    schemaFormDecoratorsProvider.defineAddOn(
+      'bootstrapDecorator',           // Name of the decorator you want to add to.
+      'intl_tel_input',                      // Form type that should render this add-on
+      'src/templates/intl_tel_input.html',  // Template name in $templateCache
+      sfBuilderProvider.stdBuilders   // List of builder functions to apply.
+    )
 
-  ngIntlTelInputProvider.set({initialCountry: 'us'})
-})
+    ngIntlTelInputProvider.set({initialCountry: 'us'})
+  }
+])
