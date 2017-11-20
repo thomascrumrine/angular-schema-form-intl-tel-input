66958f81142b2ec603a152fafdd278b8d88bccbc
thomascrumrine <tommy@apruve.com>
2017-11-20 11:54:54 -0600

    Using proper classes

--- dist/intl_tel_input.js
+++ dist/intl_tel_input.js
@@ -1,13 +1,13 @@
-angular.module('templates', []).run(['$templateCache', function($templateCache) {$templateCache.put('src/templates/intl_tel_input.html','<div class="form-group">\n  <label>{{form.title}}</label>\n  <div ng-class="{\'input-group\': (form.fieldAddonLeft || form.fieldAddonRight)}">\n    <span ng-if="form.fieldAddonLeft"\n          class="input-group-addon"\n          ng-bind-html="form.fieldAddonLeft"></span>\n    <input name=\'tel\'\n           class="form-control"\n           sf-field-model\n           ng-intl-tel-input\n           ng-model=\'form.phone\'\n           schema-validate="form"\n           type="tel"\n           data-selected-country="form.telCountry"\n           data-initial-country="us" />\n    </input>\n    <span ng-if="form.fieldAddonLeft"\n          class="input-group-addon"\n          ng-bind-html="form.fieldAddonLeft"></span>\n  </div>\n\n  <span class="help-block" sf-message>{{ (hasError() && errorMessage(schemaError())) || form.description}}</span>\n</div>\n');}]);
+angular.module('templates', []).run(['$templateCache', function($templateCache) {$templateCache.put('src/templates/intl_tel_input.html','<div class="form-group {{form.htmlClass}} schema-form-intl-tel-input"\n     ng-class="{\'has-error\': form.disableErrorState !== true && hasError(), \'has-success\': form.disableSuccessState !== true && hasSuccess(), \'has-feedback\': form.feedback !== false, \'required\': form.required === true}">\n  <label class="control-label" ng-show="showTitle()">{{form.title}}</label>\n  <input name="{{form.key.slice(-1)[0]}}"\n         class="form-control {{form.fieldHtmlClass}}"\n         sf-field-model\n         ng-intl-tel-input\n         ng-model="$$value$$"\n         schema-validate="form"\n         type="tel"\n         data-selected-country="form.telCountry"\n         data-initial-country="us"\n         sf-changed="form"\n         ng-disabled="form.readonly">\n  </input>\n\n  <div class="help-block" sf-message="form.description"></div>\n</div>\n');}]);
 angular.module('intlTelInput', ['schemaForm', 'templates', 'ngIntlTelInput']).config([
   'schemaFormDecoratorsProvider', 'sfBuilderProvider', 'ngIntlTelInputProvider',
   function(schemaFormDecoratorsProvider, sfBuilderProvider, ngIntlTelInputProvider) {
 
     schemaFormDecoratorsProvider.defineAddOn(
-      'bootstrapDecorator',           // Name of the decorator you want to add to.
-      'intl_tel_input',                      // Form type that should render this add-on
-      'src/templates/intl_tel_input.html',  // Template name in $templateCache
-      sfBuilderProvider.stdBuilders   // List of builder functions to apply.
+      'bootstrapDecorator',
+      'intl_tel_input',
+      'src/templates/intl_tel_input.html',
+      sfBuilderProvider.stdBuilders
     )
 
     ngIntlTelInputProvider.set({initialCountry: 'us'})
--- src/module.js
+++ src/module.js
@@ -3,10 +3,10 @@ angular.module('intlTelInput', ['schemaForm', 'templates', 'ngIntlTelInput']).co
   function(schemaFormDecoratorsProvider, sfBuilderProvider, ngIntlTelInputProvider) {
 
     schemaFormDecoratorsProvider.defineAddOn(
-      'bootstrapDecorator',           // Name of the decorator you want to add to.
-      'intl_tel_input',                      // Form type that should render this add-on
-      'src/templates/intl_tel_input.html',  // Template name in $templateCache
-      sfBuilderProvider.stdBuilders   // List of builder functions to apply.
+      'bootstrapDecorator',
+      'intl_tel_input',
+      'src/templates/intl_tel_input.html',
+      sfBuilderProvider.stdBuilders
     )
 
     ngIntlTelInputProvider.set({initialCountry: 'us'})
